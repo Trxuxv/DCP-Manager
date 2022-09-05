5176dad4577f651be0b6a2b7c2c0e3013b0c501c
debora.maciel@techer.com.br <debora.maciel@techer.com.br>
2022-09-05 12:26:02 -0300

    Landing page

--- pages/home/landing.vue
+++ pages/home/landing.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    Landing
+  <div class="w-full h-full bg-red-200" id="background">
+    Landinga
   </div>
 </template>
+<style>
+#background{
+  background-image: url('./../../static/vectors/3.jpg');
+}
+
+</style>
